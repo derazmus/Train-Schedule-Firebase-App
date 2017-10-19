3b16b0910dadd4dfb75c5b20b0b127d813416390
Denise Erazmus <denise.erazmus@gmail.com>
2017-10-19 16:22:59 -0500

    data to database and on html

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -11,32 +11,78 @@
 
   firebase.initializeApp(config);
 
+  //a variable to referance the database
+
   var database = firebase.database();
+
+  //variables from form
+
+var trainName = "";
+var destination = "";
+var firstTrain = 0;
+var frequency = 0;
 
   // 2. Button for adding Trains
 $("#submit").on("click", function(event) {
   event.preventDefault();
 
-  // Grabs user input
-  var trainName = $("#trainName").val();
-  var destination = $("#destination").val();
-  var firstTrain = $("#firstTrain").val();
-  var frequency = $("#frequency").val();
 
-  // Creates local "temporary" object for holding traub data
-  var newTrain = {
-    TrainName: trainName,
-    destination: destination,
-    firstTrain: firstTrain,
-    frequency: frequency
-  };
+	trainName = $("#trainName").val().trim();
 
-  // Uploads employee data to the database
-  database.ref().push(newTrain);
+	destination = $("#destination").val().trim();
 
-  // Logs everything to console
-  console.log(trainName.trainName);
-  console.log(destination.destination);
-  console.log(firstTrain.firstTrain);
-  console.log(frequency.frequency);
+	firstTrain = $("#firstTrain").val().trim();
+
+	frequency = $("#frequency").val().trim();
+
+//console log worked up to here. 
+
+  database.ref().push({
+
+	    trainName: trainName,
+	    destination: destination,
+	    firstTrain: firstTrain,
+	    frequency: frequency
+
+	});
 });
+
+database.ref().on("child_added", function(childSnapshot) {
+
+ 
+
+  		//monthsWorked = moment().diff(moment(startDate),"months");
+  		//totalBilled = monthsWorked * (childSnapshot.val().monthlyRate);
+
+
+      	var myRow = $("<tr>");
+      	var myData = $("<td>");
+      	myData.html(childSnapshot.val().trainName);
+      	myRow.append(myData);
+
+       	myData = $("<td>");
+      	myData.html(childSnapshot.val().destination);
+      	myRow.append(myData);
+
+       	//myData = $("<td>");
+      	//myData.html(childSnapshot.val().firstTrain);
+      	//myRow.append(myData);
+
+       	myData = $("<td>");
+      	myData.html(childSnapshot.val().frequency);
+      	myRow.append(myData);
+
+       	//myData = $("<td>");
+      	//myData.html(totalBilled);
+      	//myRow.append(myData);
+   
+	    $("tbody").append(myRow);
+
+	     // Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+
+});
+
+
+
